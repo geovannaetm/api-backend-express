254e4a67df481616f0bb4a30e600872d8e4bf066
geovanna Emilly <geovanna.t@aluno.ifsp.edu.br>
2025-11-11 16:52:50 -0300

    ex

--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -4,10 +4,12 @@ import { listeProductController } from '../controllers/product/listeProductContr
 import { getByIdProductController } from '../controllers/product/getByIdProductController.js'
 import { editProductController } from '../controllers/product/editProductController.js'
 import { deleteProductController } from '../controllers/product/deleteProductController.js'
+import { authenticator } from '../middleware/authenticator.js'
 
 const router = express.Router()
 
 router.post('/', createProductController)
+router.use(authenticator)
 router.get('/', listeProductController)
 router.get('/:id', getByIdProductController)
 router.put('/:id', editProductController)
--- src/routers/profileRouter.js
+++ src/routers/profileRouter.js
@@ -4,7 +4,7 @@ import { listeProfileController } from '../controllers/profile/listeProfileContr
 import { getProfileByIdController } from '../controllers/profile/getByIdProfileController.js'
 import { editProfileController } from '../controllers/profile/editProfileController.js'
 import { deleteProfileController } from '../controllers/profile/deleteProfileController.js'
-
+import { authenticator } from '../middleware/authenticator.js' 
 
 const router = express.Router()
 
@@ -12,6 +12,7 @@ const router = express.Router()
 router.post('/', createProfileController)
 router.get('/', listeProfileController)
 router.get('/:id', getProfileByIdController)
+router.use(authenticator)
 router.put('/:id', editProfileController)
 router.delete('/:id', deleteProfileController)
 
